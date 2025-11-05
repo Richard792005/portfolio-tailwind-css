fda2e8446ec432a965651a12e3d66c8c52849248
Richard792005 <mikaellyaldriyan@gmail.com>
2025-11-06 05:50:21 +0700

    Dark Mode Feature

--- dist/script.js
+++ dist/script.js
@@ -36,3 +36,44 @@ window.onscroll = function () {
     tombolBackToTop.classList.remove('flex')
   }
 }
+
+
+// Dark mode
+
+const tombolDark = document.querySelector('#dark-toggle')
+const html = document.querySelector('html')
+const circle = document.querySelector('#circle')
+
+const darkMode = () => {
+    html.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+    tombolDark.checked = true
+  }
+
+  const lightMode = () => {
+      html.removeAttribute('data-theme')
+      localStorage.setItem('theme', 'light')
+      tombolDark.checked = false
+}
+
+let tema = localStorage.getItem('theme')
+if (!tema) {
+  localStorage.setItem('theme', 'light')
+}
+
+if (tema === 'dark') {
+  darkMode()
+
+} else {
+  lightMode()
+}
+
+tombolDark.addEventListener('click', function(event) {
+  tema = localStorage.getItem('theme')
+  if(tema === 'light') {
+      darkMode()
+  } else if (tema === 'dark') {
+    lightMode()
+  }
+})
+

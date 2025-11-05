787cef6a3fd81b797d3f60b759bea20c8618f2a4
Richard792005 <mikaellyaldriyan@gmail.com>
2025-11-05 17:34:48 +0700

    Membuat tombol BackToTop & fixed button hamburger

--- dist/script.js
+++ dist/script.js
@@ -3,20 +3,36 @@ const navMenu = document.querySelector('#nav-menu')
 
 
 
-hamburger.addEventListener('click', function(event) {
+hamburger.addEventListener('click', function (event) {
   hamburger.classList.toggle('hamburger-active')
   navMenu.classList.toggle('hidden')
+
+})
+// buat agr saat window di klikk nav menu hilang
+
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active')
+    navMenu.classList.add('hidden')
+  }
 })
 
 
+
 // navbar-fixed
-window.onscroll = function() {
+window.onscroll = function () {
   const header = document.querySelector('header')
   const fixedNav = header.offsetTop
+  const tombolBackToTop = document.getElementById('to-top')
+  console.log(fixedNav)
 
-  if (window.pageYOffset > fixedNav ) {
+  if (window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fixed')
+    tombolBackToTop.classList.remove('hidden')
+    tombolBackToTop.classList.add('flex')
   } else {
     header.classList.remove('navbar-fixed')
+    tombolBackToTop.classList.add('hidden')
+    tombolBackToTop.classList.remove('flex')
   }
 }
